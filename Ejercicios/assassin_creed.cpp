f83daea7e567303fcce73ca4c77309f2f97325c7
DanlexSV <danilibra15@hotmail.com>
2024-11-29 05:17:44 +0100

    Assassin funcionando correctamente

--- Ejercicios/assassin_creed.cpp
+++ Ejercicios/assassin_creed.cpp
@@ -1,54 +1,71 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
+#include <generator>
 #include <string>
 #include <vector>
 #include <ranges>
+#include <format>
+
+// Librerias no Estandar
 #include <nlohmann/json.hpp>
-#include <algorithm>
 
 using namespace std;
 
 struct Target {
-    string name;
-    bool achieved;
+	string name;
+	bool achieved;
 };
 
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Target, name, achieved)
 
-auto same_name(Target t1, Target t2) -> bool {
-    return t1.name == t2.name;
-}
+///////////////////////////////////////////////////////////////////////
+	
+	template<typename T>
+	auto get_from_jsonl(string file) -> generator<T> {
+		auto ifs = ifstream{file, ios::binary};
+
+		if (!ifs) {
+			throw ios::failure{"Unable to open the JSON LINES file"};
+		}
+
+		auto ln = string{};
+		while (getline(ifs, ln)) {
+			auto trgt = nlohmann::json::parse(ln).get<T>();
+			co_yield trgt;
+		}
+	}
+
+template<typename T>
+struct Rectangle {
+	T width;
+	T height;
+};
 
 auto main() -> int {
-    auto targets = vector<Target>{};
+	auto r1 = Rectangle{5.6, 1.23};
+	auto r2 = Rectangle{7.87f, 100.2f};
 
-    auto ifs = ifstream{"../military_camp.jsonl", ios::binary};
-    if (!ifs) {
-        cout << "Incapaz de abrir el fichero JSON Lines" << endl;
-        return EXIT_FAILURE;
-    }
+	cout << format("base: {}, altura: {}", r1.width, r1.height) << endl;
+	cout << format("base: {}, altura: {}", r2.width, r2.height) << endl;
 
-    auto ln = string{};
-    while (getline(ifs, ln)) {
-        auto trgt = nlohmann::json::parse(ln).get<Target>();
-        targets.push_back(trgt);
-    }
-    ifs.close();
 
-    ranges::sort(targets, {}, &Target::name);
+	auto targets = vector<Target>{};
 
-    auto grouped_targets = vector<vector<Target>>{};
-    for (const auto& target : targets) {
-        if (grouped_targets.empty() || grouped_targets.back().front().name != target.name) {
-            grouped_targets.push_back({});
-        }
-        grouped_targets.back().push_back(target);
-    }
+	for (Target trgt : get_from_jsonl<Target>("../military_camp.jsonl")) {
+		targets.push_back(trgt);
+	}
 
-    for (const auto& group : grouped_targets) {
-        cout << "tipo: " << group.front().name << endl;
-    }
+	ranges::sort(targets, {}, &Target::name);
 
-    return EXIT_SUCCESS;
+	auto same_name = [](Target const& t1, Target const& t2){ return t1.name == t2.name; };
+
+	for (auto target_chunk : targets | views::chunk_by(same_name)) {
+		auto const name = ranges::begin(target_chunk)->name;
+		auto const achv = ranges::count_if(target_chunk, &Target::achieved);
+		auto const totl = ranges::distance(target_chunk);
+		cout << format("{:>15} --> {}/{}", name, achv, totl) << endl;
+	}
+
+	return EXIT_SUCCESS;
 }
-
